101816d09493cd162cf38dceddcac609e7ce4259
Abdou Abarchi Aboubacar <57944650+AbdouAbarchiAboubacar@users.noreply.github.com>
2023-03-15 14:22:30 +0100

    Add proguard-rules.pro

--- lib/ui/screens/home.dart
+++ lib/ui/screens/home.dart
@@ -78,6 +78,7 @@ class _HomeState extends State<Home> with AutomaticKeepAliveClientMixin {
 
   @override
   Widget build(BuildContext context) {
+    super.build(context);
     return Scaffold(
         backgroundColor: Colors.white,
         body: Stack(
